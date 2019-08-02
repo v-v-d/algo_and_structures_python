eff922c039c2e2635770af8068cb68f3da38da43
v-v-d <v-v-d@bk.ru>
2019-08-02 17:40:42 +0300

    task №6 done

--- Lesson_2/6.py
+++ Lesson_2/6.py
@@ -5,3 +5,53 @@
 число, чем то, что загадано. Если за 10 попыток число не отгадано,
 то вывести загаданное число.
 """
+import random
+
+
+# Рекурсия
+def guess_the_number_game(num, attempts, attempt=1):
+    print(f'Попытка №{attempt}')
+    try:
+        user_answer = int(input('Угадайте число от 0 до 100: '))
+
+        if user_answer == num:
+            print('Вы угадали!')
+            exit(0)
+        if user_answer < num:
+            print(f'Загаданное число больше {user_answer}')
+        else:
+            print(f'Загаданное число меньше {user_answer}')
+    except ValueError:
+        print('Ошибка! Необходимо ввести целое число от 0 до 100')
+    if attempt < attempts:
+        guess_the_number_game(num, attempts, attempt + 1)
+    else:
+        print(f'Вы проиграли. Было загадано число {num}')
+        exit(0)
+
+
+random_number = random.randint(0, 100)
+num_of_attempts = 10
+
+# # Вывод рекурсии
+# print(guess_the_number_game(random_number, num_of_attempts))
+
+# Цикл
+for attempt in range(num_of_attempts):
+    print(f'Попытка №{attempt + 1}')
+    try:
+        user_answer = int(input('Угадайте число от 0 до 100: '))
+
+        if user_answer == random_number:
+            print('Вы угадали!')
+            break
+        if user_answer < random_number:
+            print(f'Загаданное число больше {user_answer}')
+        else:
+            print(f'Загаданное число меньше {user_answer}')
+    except ValueError:
+        print('Ошибка! Необходимо ввести целое число от 0 до 100')
+
+    if attempt == num_of_attempts - 1:
+        print(f'Вы проиграли. Было загадано число {random_number}')
+        break
