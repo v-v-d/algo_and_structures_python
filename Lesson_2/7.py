b99d5749ddffbe83be054ba053f9b9e5a0d3dfd6
v-v-d <v-v-d@bk.ru>
2019-08-02 19:28:03 +0300

    task №7 done

--- Lesson_2/7.py
+++ Lesson_2/7.py
@@ -3,3 +3,36 @@
 натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
  где n - любое натуральное число.
 """
+
+
+# Рекурсия
+def prove_equality(num, result=0):
+    result += num
+    if num != 0:
+        return prove_equality(num - 1, result)
+    return result
+
+
+try:
+    num = int(input('Введите число: '))
+    check = num * (num + 1) / 2
+
+    result = prove_equality(num)
+
+    print('Равенство выполняется') if result == check else print('Равенство не выполняется')
+except ValueError:
+    print('Ошибка! Необходимо ввести целое число')
+
+
+# Цикл
+try:
+    num = int(input('Введите число: '))
+    check = num * (num + 1) / 2
+
+    result = 0
+    for i in range(num):
+        result += i + 1
+
+    print('Равенство выполняется') if result == check else print('Равенство не выполняется')
+except ValueError:
+    print('Ошибка! Необходимо ввести целое число')
