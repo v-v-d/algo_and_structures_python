17e95a578a89a64452aa5024ec220f39e5bd9d75
v-v-d <v-v-d@bk.ru>
2019-08-06 16:00:00 +0300

    task №3 done

--- Lesson_3/3.py
+++ Lesson_3/3.py
@@ -1,2 +1,16 @@
 #3.	В массиве случайных целых чисел поменять местами минимальный
 # и максимальный элементы.
+import random
+
+lst = [random.randint(-10, 10) for i in range(20)]
+
+min_index, max_index = 0, 0
+min_num, max_num = lst[0], lst[0]
+for i, num in enumerate(lst):
+    if num < min_num:
+        min_num = num
+        min_index = i
+    if num > max_num:
+        max_num = num
+        max_index = i
+lst[min_index], lst[max_index] = lst[max_index], lst[min_index]
