1eea552df4bd5c805739b3c7bbfad07f12a92648
v-v-d <v-v-d@bk.ru>
2019-08-06 19:58:27 +0300

    task №3 prints was added

--- Lesson_3/3.py
+++ Lesson_3/3.py
@@ -4,13 +4,17 @@ import random
 
 lst = [random.randint(-10, 10) for i in range(20)]
 
+print(lst)
+
 min_index, max_index = 0, 0
 min_num, max_num = lst[0], lst[0]
 for i, num in enumerate(lst):
     if num < min_num:
         min_num = num
         min_index = i
-    if num > max_num:
+    elif num > max_num:
         max_num = num
         max_index = i
 lst[min_index], lst[max_index] = lst[max_index], lst[min_index]
+
+print(lst)
