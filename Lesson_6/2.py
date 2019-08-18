4558338e4b8ba5ebc45c4716d2151f4de62ff820
v-v-d <v-v-d@bk.ru>
2019-08-18 22:38:51 +0300

    task 1 sys info added

--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -7,3 +7,35 @@
 """
 
 
+class HexOperation1:
+    def __init__(self, num_first, num_second):
+        self.num_first = num_first
+        self.num_second = num_second
+
+    def __add__(self, other):
+        return list(hex(int(''.join(self.num_first), 16) + int(''.join(other.num_second), 16)))[2:]
+
+    def __mul__(self, other):
+        return list(hex(int(''.join(self.num_first), 16) * int(''.join(other.num_second), 16)))[2:]
+
+
+class HexOperation2:
+    __slots__ = ('num_first', 'num_second')
+
+    def __init__(self, num_first, num_second):
+        self.num_first = num_first
+        self.num_second = num_second
+
+    def __add__(self, other):
+        return list(hex(int(''.join(self.num_first), 16) + int(''.join(other.num_second), 16)))[2:]
+
+    def __mul__(self, other):
+        return list(hex(int(''.join(self.num_first), 16) * int(''.join(other.num_second), 16)))[2:]
+
+
+hex_num_first = list(input('Введите первое шеснадцатиричное число: '))
+hex_num_second = list(input('Введите второе шеснадцатиричное число: '))
+
+res_sum = HexOperation1(hex_num_first, hex_num_second) + HexOperation1(hex_num_first, hex_num_second)
+res_mul = HexOperation1(hex_num_first, hex_num_second) * HexOperation1(hex_num_first, hex_num_second)
+print(f'Сумма чисел = {res_sum}\nПроизведение чисел = {res_mul}')
