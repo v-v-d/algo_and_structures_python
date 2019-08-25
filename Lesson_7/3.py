256bf8764dcd7e1cfe4d240f8132c300c2f2b88f
v-v-d <v-v-d@bk.ru>
2019-08-25 12:48:33 +0300

    task 3 done

--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,38 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+import random
+import timeit
+
+
+def get_median(lst):
+    left, middle, right = [], [], []
+    pivot = len(lst) // 2
+
+    for el in lst:
+        if el < lst[pivot]:
+            left.append(el)
+        elif el > lst[pivot]:
+            right.append(el)
+        else:
+            middle.append(el)
+
+    if lst == left + middle + right:
+        # print(left + middle + right)
+        return lst[pivot]
+    else:
+        return get_median(left + middle + right)
+
+
+if __name__ == '__main__':
+    m = random.randint(2, 20)
+
+    random_list = [random.randint(-100, 100) for _ in range(2 * m + 1)]
+
+    print(random_list)
+    median = get_median(random_list)
+    print(f'Медиана списка: {median}')
+
+    print(timeit.timeit('get_median(random_list)',
+                        setup='from __main__ import get_median, m, random_list',
+                        number=1000))
